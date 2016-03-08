91db501768f5f65863d248507380d7a891e0ba67
Jonathan Arnett <jonarnett90@gmail.com>
2016-03-07 23:18:32 -0500

    Fixed due date bug on update

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,28 +2,38 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
+'use strict';
+
 $(function() {
+    let stash = null;
+
+    const fields = [ "#task_due_date_1i", "#task_due_date_2i", "#task_due_date_3i" ];
+
     var hideOrShowDueDate = function() {
         const checked = $("#task_due").is(':checked');
 
         if (checked) {
-            const date = new Date();
+            // If the values are empty, populate them
+            if ($(fields[0]).val() === null) {
+                // Try stash, resort to current date
+                const date = stash || new Date();
 
-            $("#task_due_date_1i").val(String(1900 + date.getYear()));
-            $("#task_due_date_2i").val(String(1 + date.getMonth()));
-            $("#task_due_date_3i").val(String(date.getDate()));
+                // Damned JavaScript dates
+                $(fields[0]).val(String(1900 + date.getYear()));
+                $(fields[1]).val(String(1 + date.getMonth()));
+                $(fields[2]).val(String(date.getDate()));
+            }
 
-            $("#task_due_date_1i").show();
-            $("#task_due_date_2i").show();
-            $("#task_due_date_3i").show();
+            // Show all fields
+            fields.forEach(function(e) { $(e).show() });
         } else {
-            $("#task_due_date_1i").val(null);
-            $("#task_due_date_2i").val(null);
-            $("#task_due_date_3i").val(null);
+            // Stash the current date when hiding, if there is one
+            if ($(fields[0]).val() !== null) {
+                stash = new Date(fields.map(function(e) { return $(e).val(); }));
+            }
 
-            $("#task_due_date_1i").hide();
-            $("#task_due_date_2i").hide();
-            $("#task_due_date_3i").hide();
+            fields.forEach(function(e) { $(e).val(null) });
+            fields.forEach(function(e) { $(e).hide() });
         }
     }
 
